7ddc22c9c31c938243b9d21571f27a66c25c274e
Ram Kumar K <ramkumar.k@aquastridetech.com>
2025-02-21 14:11:19 +0530

    MY portfolio changes...

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    emailJS: {
+      serviceID: 'service_9v9jd6v',
+      templateID: 'template_4qc1tri',
+      publicKey: 'zW4h_k4CUS6pIinyW'
+    }
+  };
+  
